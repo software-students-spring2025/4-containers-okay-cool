fcc190f6870e45e3560c0fbe6fe160fffd9b96a9
bdeweesevans <bdeweesevans@gmail.com>
2025-04-06 22:15:54 -0400

    dealing with non-recognition of opencv cv2 library
    the linter does not recognize cv2 function usage, so we need to silence the linter. also changed num_faces so that it is not recognized as a constant, and added a newline at the end of the file

--- machine-learning-client/client.py
+++ machine-learning-client/client.py
@@ -13,20 +13,21 @@ from mtcnn import MTCNN
 start_time = time.time()
 
 # Read image input
-IMAGE_NAME = "1"
+IMAGE_NAME = "7"
 IMAGE_PATH = f"./images/{IMAGE_NAME}.jpg"
 IMAGE_PATH_PATCHED = f"./images/{IMAGE_NAME}-patched.jpg"
-img = cv2.imread(IMAGE_PATH)
+img = cv2.imread(IMAGE_PATH)	# pylint: disable=E1101
 
 # MTCNN expects RGB, not BGR, so we are converting that here
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)	# pylint: disable=E1101
 
 # Detect faces w/ MTCNN & extract their count
 detector = MTCNN()
 faces = detector.detect_faces(img_rgb)
-num_faces = 0
 if faces:
     num_faces = len(faces)
+else:
+	num_faces = 0
 
 # Creating side-by-side sub-plots
 fig, axs = plt.subplots(1, 2, figsize=(20, 10), gridspec_kw={'wspace': 0.05})
@@ -42,7 +43,7 @@ img_with_patches = img.copy()
 for face in faces:
     x, y, w, h = face['box']
     # Draw red borders
-    img_with_patches = cv2.rectangle(
+    img_with_patches = cv2.rectangle(	# pylint: disable=E1101
         img_with_patches,
         (x, y),
         (x + w, y + h),
@@ -50,7 +51,7 @@ for face in faces:
         2               # boundary box border width
     )
     # Draw blue fills
-    img_with_patches = cv2.rectangle(
+    img_with_patches = cv2.rectangle(	# pylint: disable=E1101
         img_with_patches,
         (x, y),
         (x + w, y + h),
@@ -59,7 +60,7 @@ for face in faces:
     )
 
 # Convert patched image to RGB for matplotlib
-img_with_patches_rgb = cv2.cvtColor(img_with_patches, cv2.COLOR_BGR2RGB)
+img_with_patches_rgb = cv2.cvtColor(img_with_patches, cv2.COLOR_BGR2RGB)	# pylint: disable=E1101
 
 # Put patched image on plot
 axs[1].imshow(img_with_patches_rgb)
@@ -67,7 +68,7 @@ axs[1].axis('off')
 axs[1].set_title("Processed Image with MTCNN Faces")
 
 # Save processed image to ./images
-cv2.imwrite(IMAGE_PATH_PATCHED, img_with_patches)
+cv2.imwrite(IMAGE_PATH_PATCHED, img_with_patches)	# pylint: disable=E1101
 
 end_time = time.time()
 elapsed_time = end_time - start_time
