b7c44fcffce8d55aa6774ee5ddea923b25be3cef
bdeweesevans <bdeweesevans@gmail.com>
2025-04-06 22:03:12 -0400

    fixing linting errors

--- machine-learning-client/client.py
+++ machine-learning-client/client.py
@@ -1,74 +1,81 @@
-import cv2
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from mtcnn import MTCNN
+"""
+This script detects faces with MTCNN,
+covering filling in their boundry boxes
+and saves the redacted image.
+"""
 import time
 
-# starting timer
+import cv2
+import matplotlib.pyplot as plt
+from mtcnn import MTCNN
+
+# Starting timer
 start_time = time.time()
 
-# read image input
-name = "image"
-path = f"./images/{name}.jpg"
-path_patched = f"./images/{name}-patched.jpg"
-img = cv2.imread(path)
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # MTCNN expects RGB
+# Read image input
+IMAGE_NAME = "1"
+IMAGE_PATH = f"./images/{IMAGE_NAME}.jpg"
+IMAGE_PATH_PATCHED = f"./images/{IMAGE_NAME}-patched.jpg"
+img = cv2.imread(IMAGE_PATH)
 
-# detect faces w/ MTCNN
+# MTCNN expects RGB, not BGR, so we are converting that here
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+# Detect faces w/ MTCNN & extract their count
 detector = MTCNN()
 faces = detector.detect_faces(img_rgb)
 num_faces = 0
 if faces:
-	num_faces = len(faces)
+    num_faces = len(faces)
 
-# side-by-side plots
+# Creating side-by-side sub-plots
 fig, axs = plt.subplots(1, 2, figsize=(20, 10), gridspec_kw={'wspace': 0.05})
 
-# show original image on plot
+# Put original image on plot
 axs[0].imshow(img_rgb)
 axs[0].axis('off')
 axs[0].set_title("Original Image")
 
-# copy for processing
+# Copy for processing
 img_with_patches = img.copy()
 
 for face in faces:
-	x, y, w, h = face['box']
-	# draw red borders
-	img_with_patches = cv2.rectangle(
-	    img_with_patches, 
-	    (x, y), 
-	    (x + w, y + h), 
-	    (0, 0, 255),	# bgr format
-	    2   # boundry box border width
-	)
-	# draw blue fills
-	img_with_patches = cv2.rectangle(
-	    img_with_patches, 
-	    (x, y), 
-	    (x + w, y + h), 
-	    (0, 255, 255),  # Blue color in BGR format
-	    -1  # -1 for fill
-	)
+    x, y, w, h = face['box']
+    # Draw red borders
+    img_with_patches = cv2.rectangle(
+        img_with_patches,
+        (x, y),
+        (x + w, y + h),
+        (0, 0, 255),    # bgr format
+        2               # boundary box border width
+    )
+    # Draw blue fills
+    img_with_patches = cv2.rectangle(
+        img_with_patches,
+        (x, y),
+        (x + w, y + h),
+        (0, 255, 255),
+        -1              # -1 for fill
+    )
 
-# convert patched image to RGB for matplotlib
+# Convert patched image to RGB for matplotlib
 img_with_patches_rgb = cv2.cvtColor(img_with_patches, cv2.COLOR_BGR2RGB)
 
-# show patched image on plot
+# Put patched image on plot
 axs[1].imshow(img_with_patches_rgb)
 axs[1].axis('off')
 axs[1].set_title("Processed Image with MTCNN Faces")
 
-# save processed image to ./images directory
-cv2.imwrite(path_patched, img_with_patches)
+# Save processed image to ./images
+cv2.imwrite(IMAGE_PATH_PATCHED, img_with_patches)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 rounded_time = round(elapsed_time, 1)
 
-# printing data
+# Printing data
 print(f"{num_faces} FACES")
 print(f"{rounded_time} SEC.")
 
-# display side by side plot
+# Display side-by-side plot
 plt.show()
